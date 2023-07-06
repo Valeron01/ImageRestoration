dd2f80fdefd04cf52dbaed51dfa099f4b9df52f9
Valeron01 <electronpochta3011@mail.ru>
2023-07-06 11:14:50 +0300

    Added unet

--- models/blocks.py
+++ models/blocks.py
@@ -1,12 +1,14 @@
+import typing
+
 from torch import nn
 
 
 class ResBlock(nn.Module):
-    def __init__(self, in_channels: int, out_channels: int):
+    def __init__(self, in_channels: int, out_channels: int, stride: int = 1):
         super().__init__()
 
         self.block = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
+            nn.Conv2d(in_channels, out_channels, 3, stride, 1, bias=False),
             nn.BatchNorm2d(out_channels),
             nn.LeakyReLU(inplace=True),
 
@@ -15,7 +17,9 @@ class ResBlock(nn.Module):
             nn.LeakyReLU(inplace=True)
         )
 
-        self.identity = nn.Identity() if in_channels == out_channels else nn.Conv2d(in_channels, out_channels, 1)
+        self.identity = nn.Identity() if in_channels == out_channels else nn.Conv2d(
+            in_channels, out_channels, stride, stride
+        )
 
     def forward(self, x):
         return self.block(x) + self.identity(x)
---
+++ models/unet.py
@@ -0,0 +1,67 @@
+import typing
+
+import torch
+from torch import nn
+from blocks import ResBlock
+
+
+class UNet(nn.Module):
+    def __init__(self, dim: int, depth: int):
+        super().__init__()
+
+        self.depth = depth
+
+        self.stem = nn.Sequential(
+            nn.Conv2d(3, dim, 7, 1, 3, bias=False),
+            nn.BatchNorm2d(dim),
+            nn.LeakyReLU(inplace=True)
+        )
+
+        self.encoder = nn.ModuleList([
+            ResBlock(
+                in_channels=2 ** current_depth * dim, out_channels=2 ** current_depth * dim * 2, stride=2
+            ) for current_depth in range(depth)
+        ])
+
+        self.decoder = nn.ModuleList([
+            # 2 * 2 is here because:
+            # we need concatenate previous stages of encoder and decoder;
+            # because of up-scaling
+            nn.Sequential(
+                ResBlock(
+                    in_channels=2 * 2 ** current_depth * dim * (2 if current_depth != depth - 1 else 1),
+                    out_channels=2 ** current_depth * dim
+                ),
+                nn.UpsamplingBilinear2d(scale_factor=2)
+            ) for current_depth in reversed(range(depth))
+        ])
+
+        self.resulting_layer = nn.Conv2d(2 * dim, 3, 1)
+
+    def forward(self, x):
+        stem = self.stem(x)
+
+        downsample_stage = stem
+        downsample_stages = [stem]
+        for donwsample_layer in self.encoder:
+            downsample_stage = donwsample_layer(downsample_stage)
+            downsample_stages.append(downsample_stage)
+
+        previous_stage = downsample_stages[-1]
+        for encoder_features, upsample_layer in zip(reversed(downsample_stages[:-1]), self.decoder):
+            new_stage = upsample_layer(previous_stage)
+            previous_stage = torch.cat([encoder_features, new_stage], dim=1)
+
+        return self.resulting_layer(previous_stage)
+
+
+
+
+
+if __name__ == '__main__':
+    model = UNet(64, 3)
+    print(model(torch.randn(1, 3, 256, 256)).shape)
+
+    # print(model.encoder)
+    # print("-" * 40)
+    # print(model.decoder)
